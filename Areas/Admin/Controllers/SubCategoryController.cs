f9a0ee9bb882118944ac6dd68ed521a4b5fc9c81
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-07-15 15:54:46 +0430

    added View model for subCategory get action

--- Areas/Admin/Controllers/SubCategoryController.cs
+++ Areas/Admin/Controllers/SubCategoryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models.ViewModels;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -24,6 +25,20 @@ namespace Spice.Areas.Admin.Controllers
         {
             var SubCategories = await _db.SubCategory.Include(s => s.Category).ToListAsync();
             return View(SubCategories);
+        }
+
+
+        //GET - CREATE
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryList =
+                    await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+            };
+            return View(model);
         }
     }
 }
