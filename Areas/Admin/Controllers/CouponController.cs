9e875a2ead96cfbb378ef14837d8d7c03882a7d7
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-07-21 21:52:01 +0430

    Fixed Image on Views

--- Areas/Admin/Controllers/CouponController.cs
+++ Areas/Admin/Controllers/CouponController.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Razor.Language.Intermediate;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
 using Spice.Models;
+using Spice.Utility;
 
 namespace Spice.Areas.Admin.Controllers
 {
     [Area("Admin")]
-
     public class CouponController : Controller
     {
-
         private readonly ApplicationDbContext _db;
 
         public CouponController(ApplicationDbContext db)
@@ -23,23 +22,15 @@ namespace Spice.Areas.Admin.Controllers
             _db = db;
         }
 
-
-        //GET -Index
         public async Task<IActionResult> Index()
         {
             return View(await _db.Coupon.ToListAsync());
         }
 
-
-        //GET - Create
-
         public IActionResult Create()
         {
             return View();
         }
-
-
-        //POST - CREATE
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -60,38 +51,30 @@ namespace Spice.Areas.Admin.Controllers
                         }
                     }
                     coupons.Picture = p1;
-
                 }
-
                 _db.Coupon.Add(coupons);
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-
             return View(coupons);
         }
 
 
-        //GET - EDIT
+        //GET Edit Coupon
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
-            var copoun = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
-            if (copoun == null)
+            var coupon = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+            if (coupon == null)
             {
                 return NotFound();
             }
-
-            return View(copoun);
+            return View(coupon);
         }
 
-
-
-        //POST - EDIT
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(Coupon coupons)
@@ -117,10 +100,8 @@ namespace Spice.Areas.Admin.Controllers
                             p1 = ms1.ToArray();
                         }
                     }
-
                     couponFromDb.Picture = p1;
                 }
-
                 couponFromDb.MinimumAmount = coupons.MinimumAmount;
                 couponFromDb.Name = coupons.Name;
                 couponFromDb.Discount = coupons.Discount;
@@ -130,11 +111,10 @@ namespace Spice.Areas.Admin.Controllers
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-
             return View(coupons);
         }
 
-        //GET - DETAILS
+
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -142,7 +122,8 @@ namespace Spice.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var coupon = await _db.Coupon.FirstOrDefaultAsync(m => m.Id == id);
+            var coupon = await _db.Coupon
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (coupon == null)
             {
                 return NotFound();
@@ -151,21 +132,18 @@ namespace Spice.Areas.Admin.Controllers
             return View(coupon);
         }
 
-
-        //GET - DELETE 
+        //GET Delete Coupon
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
             var coupon = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
             if (coupon == null)
             {
                 return NotFound();
             }
-
             return View(coupon);
         }
 
